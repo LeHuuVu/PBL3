0d58f8417c74925bbc48c1a0e471dc6cc66d2718
unknown <lehuuvupro123@gmail.com>
2021-08-29 08:05:08 +0700

    code pbl day du

---
+++ Form2.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace HuffmanCoding
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvHistory = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.butSearch = new System.Windows.Forms.Button();
+            this.butDel = new System.Windows.Forms.Button();
+            this.butExit = new System.Windows.Forms.Button();
+            this.butSort = new System.Windows.Forms.Button();
+            this.cbbSort = new System.Windows.Forms.ComboBox();
+            this.butShow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvHistory
+            // 
+            this.dgvHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistory.Location = new System.Drawing.Point(70, 86);
+            this.dgvHistory.Name = "dgvHistory";
+            this.dgvHistory.RowHeadersWidth = 51;
+            this.dgvHistory.RowTemplate.Height = 24;
+            this.dgvHistory.Size = new System.Drawing.Size(645, 266);
+            this.dgvHistory.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(89, 29);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(118, 22);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // butSearch
+            // 
+            this.butSearch.Location = new System.Drawing.Point(235, 25);
+            this.butSearch.Name = "butSearch";
+            this.butSearch.Size = new System.Drawing.Size(104, 30);
+            this.butSearch.TabIndex = 3;
+            this.butSearch.Text = "Search";
+            this.butSearch.UseVisualStyleBackColor = true;
+            this.butSearch.Click += new System.EventHandler(this.butSearch_Click);
+            // 
+            // butDel
+            // 
+            this.butDel.Location = new System.Drawing.Point(338, 385);
+            this.butDel.Name = "butDel";
+            this.butDel.Size = new System.Drawing.Size(104, 31);
+            this.butDel.TabIndex = 4;
+            this.butDel.Text = "Delete";
+            this.butDel.UseVisualStyleBackColor = true;
+            this.butDel.Click += new System.EventHandler(this.butDel_Click);
+            // 
+            // butExit
+            // 
+            this.butExit.Location = new System.Drawing.Point(570, 385);
+            this.butExit.Name = "butExit";
+            this.butExit.Size = new System.Drawing.Size(116, 30);
+            this.butExit.TabIndex = 4;
+            this.butExit.Text = "Exit";
+            this.butExit.UseVisualStyleBackColor = true;
+            this.butExit.Click += new System.EventHandler(this.butExit_Click);
+            // 
+            // butSort
+            // 
+            this.butSort.Location = new System.Drawing.Point(427, 25);
+            this.butSort.Name = "butSort";
+            this.butSort.Size = new System.Drawing.Size(104, 30);
+            this.butSort.TabIndex = 6;
+            this.butSort.Text = "Sort";
+            this.butSort.UseVisualStyleBackColor = true;
+            this.butSort.Click += new System.EventHandler(this.butSort_Click);
+            // 
+            // cbbSort
+            // 
+            this.cbbSort.FormattingEnabled = true;
+            this.cbbSort.Items.AddRange(new object[] {
+            "TruocMaHoa",
+            "SauMaHoa",
+            "ThoiGian"});
+            this.cbbSort.Location = new System.Drawing.Point(565, 29);
+            this.cbbSort.Name = "cbbSort";
+            this.cbbSort.Size = new System.Drawing.Size(121, 24);
+            this.cbbSort.TabIndex = 7;
+            // 
+            // butShow
+            // 
+            this.butShow.Location = new System.Drawing.Point(91, 384);
+            this.butShow.Name = "butShow";
+            this.butShow.Size = new System.Drawing.Size(116, 30);
+            this.butShow.TabIndex = 8;
+            this.butShow.Text = "Show";
+            this.butShow.UseVisualStyleBackColor = true;
+            this.butShow.Click += new System.EventHandler(this.butShow_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 450);
+            this.Controls.Add(this.butShow);
+            this.Controls.Add(this.cbbSort);
+            this.Controls.Add(this.butSort);
+            this.Controls.Add(this.butExit);
+            this.Controls.Add(this.butDel);
+            this.Controls.Add(this.butSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.dgvHistory);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvHistory;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button butSearch;
+        private System.Windows.Forms.Button butDel;
+        private System.Windows.Forms.Button butExit;
+        private System.Windows.Forms.Button butSort;
+        private System.Windows.Forms.ComboBox cbbSort;
+        private System.Windows.Forms.Button butShow;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HuffmanCoding
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void butShow_Click(object sender, EventArgs e)
+        {
+            QLDB_DACK db = new QLDB_DACK();
+            var query = from p in db.Histories select p;            
+            dgvHistory.DataSource = query.ToList();            
+        }
+        private void butSearch_Click(object sender, EventArgs e)
+        {
+            if (txtSearch.Text == "")
+            {
+                MessageBox.Show("Ban phai nhap chuoi can tim!");
+            }
+            else
+            {
+                QLDB_DACK db = new QLDB_DACK();
+                var query = db.Histories.Where(p => p.TruocMaHoa.Contains(txtSearch.Text) || p.SauMaHoa.Contains(txtSearch.Text));
+                dgvHistory.DataSource = query.ToList();
+            }            
+        }
+        private void butDel_Click(object sender, EventArgs e)
+        {
+            if(dgvHistory.SelectedRows.Count > 0)
+            {
+                QLDB_DACK db = new QLDB_DACK();
+                foreach(DataGridViewRow i in dgvHistory.SelectedRows)
+                {
+                    int STT = Convert.ToInt32(i.Cells["STT"].Value);                    
+                    History Mydel = db.Histories.Find(STT);                    
+                    db.Histories.Remove(Mydel);
+                    db.SaveChanges();
+                }
+                var query = from p in db.Histories select p;
+                dgvHistory.DataSource = query.ToList();
+            }
+            else
+            {
+                MessageBox.Show("Ban phai nhap dong can xoa!");
+            }
+        }
+        private void butExit_Click(object sender, EventArgs e)
+        {            
+            this.Dispose();
+        } 
+        private void butSort_Click(object sender, EventArgs e)
+        {
+            if (cbbSort.SelectedIndex == -1)
+            {
+                MessageBox.Show("Ban phai chon doi tuong can sap xep!");
+            }
+            else
+            {
+                QLDB_DACK db = new QLDB_DACK();
+                List<History> history = new List<History>();
+                history = db.Histories.ToList();
+                string item = cbbSort.SelectedItem.ToString();
+                if (item == "TruocMaHoa") dgvHistory.DataSource = history.OrderBy(P => P.TruocMaHoa).ToList();
+                if (item == "SauMaHoa") dgvHistory.DataSource = history.OrderBy(P => P.SauMaHoa).ToList();
+                if (item == "ThoiGian") dgvHistory.DataSource = history.OrderBy(P => P.ThoiGian).ToList();
+            }            
+        }       
+    }
+}
